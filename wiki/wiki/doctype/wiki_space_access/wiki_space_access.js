461e6a197926cba6fdf13c76fb6c9790f3ba41b4
inxeoz <inxeoz@inxeoz.com>
2025-09-15 17:18:26 +0530

    updated func

--- wiki/wiki/doctype/wiki_space_access/wiki_space_access.js
+++ wiki/wiki/doctype/wiki_space_access/wiki_space_access.js
@@ -1,10 +1,11 @@
 frappe.ui.form.on('Wiki Space Access', {
-    refresh: function (frm) {
-        // Show the current wiki_space link while refreshing
-        if (frm.doc.wiki_space) {
-            frappe.msgprint(`Current Wiki Space: ${frm.doc.wiki_space}`);
-        }
+    onload: function (frm) {
+        // store the initial wiki_space value when form loads
+        frm.old_wiki_space = frm.doc.wiki_space;
+    },
 
+    refresh: function (frm) {
+    
         // Set query on 'page' field inside child table 'access_list'
         frm.set_query('page', 'access_list', function (doc, cdt, cdn) {
             return {
@@ -17,31 +18,48 @@ frappe.ui.form.on('Wiki Space Access', {
     },
 
     wiki_space: function (frm) {
+        let new_value = frm.doc.wiki_space;
+        let old_value = frm.old_wiki_space || null;
 
-        if (frm.doc.access_list) {
-
-            frappe.confirm('access list will be cleared , Are you sure you want to proceed?',
-                () => {
-                    // action to perform if Yes is selected
-                         frm.doc.access_list = []
-                }, () => {
-                    // action to perform if No is selected
-
-                    return
-                    
-                })
-       
+        // If value didn't change, just return
+        if (new_value === old_value) {
+            return;
         }
 
-        frappe.db.get_doc('Wiki Space', frm.doc.wiki_space).then(doc => {
-            console.log('Wiki Space Route:', doc.route);
-            frappe.msgprint('Wiki Space Route: ' + doc.route);
+        // If access_list has rows, confirm before clearing
+        if (frm.doc.access_list && frm.doc.access_list.length > 0) {
+            frappe.confirm(
+                'The access list will be cleared. Are you sure you want to proceed?',
+                () => {
+                    // ✅ User clicked Yes → clear access_list and continue
+                    frm.clear_table("access_list");
+                    frm.refresh_field("access_list");
 
-            // save it on the form (not global variable)
-            frm.route_value = doc.route;
-
-            // re-run queries so child table gets updated with latest filter
-            frm.refresh_field('access_list');
-        });
+                    fetch_wiki_space(frm);
+                    frm.old_wiki_space = new_value; // update old value after confirm
+                },
+                () => {
+                    // ❌ User clicked No → revert wiki_space to old value
+                    frm.set_value("wiki_space", old_value);
+                }
+            );
+        } else {
+            // No access_list → just continue
+            fetch_wiki_space(frm);
+            frm.old_wiki_space = new_value; // update old value
+        }
     }
 });
+
+// Helper function to fetch Wiki Space route
+function fetch_wiki_space(frm) {
+    frappe.db.get_doc('Wiki Space', frm.doc.wiki_space).then(doc => {
+        console.log('Wiki Space Route:', doc.route);
+    
+        // Save route on the form
+        frm.route_value = doc.route;
+
+        // Re-run queries so child table gets updated with latest filter
+        frm.refresh_field('access_list');
+    });
+}
